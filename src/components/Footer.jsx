14444a3b414cc5c1a8704351d5d1c9b3aa3ae24a
kartikgoyal89 <kartikgoyal794@gmail.com>
2023-08-05 23:32:03 +0530

    added files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechStart</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://www.youtube.com" target={"blank"}>Youtube</a>
+                <a href="https://www.instagram.com" target={"blank"}>Insagram</a>
+                <a href="https://www.github.com" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
